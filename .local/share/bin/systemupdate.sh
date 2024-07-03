ef93c089e11acea056abcd69987e136d627ed88b
Tensaiz <matevy1@gmail.com>
2024-07-03 21:09:36 +0200

    Add sys update, update transparency, update waybar with specs

---
+++ .local/share/bin/systemupdate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Check for AUR updates
+aur=$(yay -Qua | wc -l)
+ofc=$((while pgrep -x checkupdates > /dev/null ; do sleep 1; done) ; checkupdates | wc -l)
+
+# Calculate total available updates
+upd=$(( ofc + aur ))
+
+[ "${1}" == upgrade ] && printf "[Official] %-10s\n[AUR]      %-10s\n" "$ofc" "$aur" && exit
+
+# Show tooltip
+if [ $upd -eq 0 ] ; then
+    # upd="" #Remove Icon completely
+    upd="󰮯"   #If zero Display Icon only
+    echo "{\"text\":\"$upd\", \"tooltip\":\" Packages are up to date\"}"
+else
+    echo "{\"text\":\"󰮯 $upd\", \"tooltip\":\"󱓽 Official $ofc\n󱓾 AUR $aur"}"
+fi
